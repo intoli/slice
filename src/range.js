75802aa82e70191a59598861729375d3d5118920
Evan Sangaline <esangaline@gmail.com>
2018-05-31 17:49:21 -0400

    Fix the default export syntax when exporting `range`.

--- src/range.js
+++ src/range.js
@@ -1,6 +1,8 @@
-export const range = (start, end) => (
+const range = (start, end) => (
   end == null ? range(0, start) :
     Array(end - start)
       .fill()
       .map((_, index) => index + start)
 );
+
+export default range;
