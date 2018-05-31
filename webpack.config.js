f5a9bef5b0ae2e4e9f0c443946cedec9f8fc0345
Evan Sangaline <esangaline@gmail.com>
2018-05-31 17:55:05 -0400

    Add a webpack config.

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+const webpack = require('webpack');
+
+
+const mode = process.env.NODE_ENV || 'development';
+
+
+module.exports = {
+  entry: {
+    index: path.resolve(__dirname, 'src', 'index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    library: 'slice',
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        enforce: 'pre',
+        loader: 'eslint-loader',
+      },
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.(gif|jpg|jpeg|png|svg)$/,
+        loader: 'url-loader',
+      },
+    ],
+  },
+  target: 'node',
+  devtool: 'source-map',
+  mode,
+};
