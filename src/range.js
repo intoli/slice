be64d862874e07522fc55835e6507bffaed6e6f2
Evan Sangaline <esangaline@gmail.com>
2018-06-01 11:32:48 -0400

    Make `range()` return a slice array.

--- src/range.js
+++ src/range.js
@@ -1,13 +1,16 @@
+import SliceArray from './slice-array';
+
+
 const range = (start, stop, step = 1) => {
   if (stop == null) {
     return range(0, start);
   } else if (step === 1) {
-    return Array(Math.max(stop - start, 0))
+    return new SliceArray(Math.max(stop - start, 0))
       .fill()
       .map((_, index) => index + start);
   }
   const isValid = value => (step > 0 ? value < stop : value > stop);
-  const values = [];
+  const values = new SliceArray();
   let currentValue = start;
   while (isValid(currentValue)) {
     values.push(currentValue);
