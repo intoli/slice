c4c80fbec31a58ff635cec55453d1dfc78dbef3e
Evan Sangaline <esangaline@gmail.com>
2018-06-01 11:33:07 -0400

    Update the `range()` tests to handle it returning a `SliceArray()`.

--- test/test-range.js
+++ test/test-range.js
@@ -5,22 +5,27 @@ import { range } from '../src';
 
 describe('range', () => {
   it('should construct a range starting at zero', () => {
-    assert.deepStrictEqual(range(4), [0, 1, 2, 3]);
+    assert.deepEqual(range(4), [0, 1, 2, 3]);
   });
 
   it('should construct a range starting above zero', () => {
-    assert.deepStrictEqual(range(1, 4), [1, 2, 3]);
+    assert.deepEqual(range(1, 4), [1, 2, 3]);
   });
 
   it('should construct a range below zero', () => {
-    assert.deepStrictEqual(range(-4, 0), [-4, -3, -2, -1]);
+    assert.deepEqual(range(-4, 0), [-4, -3, -2, -1]);
   });
 
   it('should handle a positive step parameter', () => {
-    assert.deepStrictEqual(range(1, 4, 2), [1, 3]);
+    assert.deepEqual(range(1, 4, 2), [1, 3]);
   });
 
   it('should handle a negative step parameter', () => {
-    assert.deepStrictEqual(range(8, 2, -3), [8, 5]);
+    assert.deepEqual(range(8, 2, -3), [8, 5]);
+  });
+
+  it('should return a SliceArray', () => {
+    assert.deepEqual(range(5)[[,,2]], [0, 2, 4]);
+    assert.deepEqual(range(5, 0, -1)[[,,-1]], range(1, 6));
   });
 });
