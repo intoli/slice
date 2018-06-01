940bb9955e05ab061399af79d85ade94608ae13c
Evan Sangaline <esangaline@gmail.com>
2018-06-01 09:18:03 -0400

    Fix typo: outut => output.

--- test/test-slice-array.js
+++ test/test-slice-array.js
@@ -6,22 +6,22 @@ import SliceArray from '../src/';
 describe('SliceArray', () => {
   it('should reverse an array', () => {
     const input = new SliceArray(1, 2, 3, 4);
-    const expectedOutut = [4, 3, 2, 1];
+    const expectedOutput = [4, 3, 2, 1];
     const output = input[[,,-1]];
-    assert.deepStrictEqual(output, expectedOutut);
+    assert.deepStrictEqual(output, expectedOutput);
   });
 
   it('should extract the even elements from an array', () => {
     const input = new SliceArray(0, 1, 2, 3, 4);
-    const expectedOutut = [0, 2, 4];
+    const expectedOutput = [0, 2, 4];
     const output = input[[,,2]];
-    assert.deepStrictEqual(output, expectedOutut);
+    assert.deepStrictEqual(output, expectedOutput);
   });
 
   it('should extract the odd elements from an array', () => {
     const input = new SliceArray(0, 1, 2, 3, 4);
-    const expectedOutut = [1, 3];
+    const expectedOutput = [1, 3];
     const output = input[[1,,2]];
-    assert.deepStrictEqual(output, expectedOutut);
+    assert.deepStrictEqual(output, expectedOutput);
   });
 });
