38e7d7873b4a5ac098cd87667c00bd33ff4db64a
Evan Sangaline <esangaline@gmail.com>
2018-06-01 11:28:27 -0400

    Add more robust range tests.

--- test/test-range.js
+++ test/test-range.js
@@ -11,4 +11,16 @@ describe('range', () => {
   it('should construct a range starting above zero', () => {
     assert.deepStrictEqual(range(1, 4), [1, 2, 3]);
   });
+
+  it('should construct a range below zero', () => {
+    assert.deepStrictEqual(range(-4, 0), [-4, -3, -2, -1]);
+  });
+
+  it('should handle a positive step parameter', () => {
+    assert.deepStrictEqual(range(1, 4, 2), [1, 3]);
+  });
+
+  it('should handle a negative step parameter', () => {
+    assert.deepStrictEqual(range(8, 2, -3), [8, 5]);
+  });
 });
