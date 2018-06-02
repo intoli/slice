7bc44cda9458f3d31fa9f380b2634265e7f22152
Evan Sangaline <esangaline@gmail.com>
2018-06-02 08:19:41 -0400

    Add a test for the `slice()` method.

--- test/test-slice.js
+++ test/test-slice.js
@@ -1,19 +1,26 @@
 import assert from 'assert';
 
-import { Slice } from '../src/';
+import { slice, Slice } from '../src/';
 
 
 describe('Slice', () => {
+  it('should not require the new keyword for the slice(start, stop, step) syntax', () => {
+    const testSlice = slice(1, 2, 3);
+    assert(testSlice.start === 1);
+    assert(testSlice.stop === 2);
+    assert(testSlice.step === 3);
+  });
+
   it('should handle the Slice(start, stop, step) constructor syntax', () => {
-    const slice = new Slice(1, 2, 3);
-    assert(slice.start === 1);
-    assert(slice.stop === 2);
-    assert(slice.step === 3);
+    const testSlice = new Slice(1, 2, 3);
+    assert(testSlice.start === 1);
+    assert(testSlice.stop === 2);
+    assert(testSlice.step === 3);
   });
 
   it('should handle the Slice(stop) constructor syntax', () => {
-    const slice = new Slice(1);
-    assert(slice.start == null);
-    assert(slice.stop === 1);
+    const testSlice = new Slice(1);
+    assert(testSlice.start == null);
+    assert(testSlice.stop === 1);
   });
 });
